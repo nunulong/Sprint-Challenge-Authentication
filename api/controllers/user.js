262aed29c69656ec0c52cab08c0c554054f229ae
Ting Wang <felix.wang.1026@gmail.com>
2018-01-20 00:20:07 -0500

    Finished the sprint w/o stretch

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,6 +4,17 @@ const bcrypt = require('bcrypt');
 const createUser = (req, res) => {
   // there should be a user object set on req
   // use that req.user object to create a user and save it to our Mongo instance.
+  const user = req.user;
+  const newUser = new User(user);
+  if (!user.username || !user.password) res.status(422).json({ error: 'Please provide username and password' });
+  newUser.save()
+    .then((user) => {
+      if (!user) res.status(422).json({ error: 'User created failed' });
+      res.status(201).json({ success: 'User created successfully', user });
+    })
+    .catch((error) => {
+      res.status(422).json({ error: error.message });
+    });
 };
 
 module.exports = {
--- api/utils/middlewares.js
+++ api/utils/middlewares.js
@@ -26,6 +26,16 @@ const encryptUserPW = (req, res, next) => {
   // TODO: Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // Once the password is encrypted using bcrypt you'll need to set a user obj on req.user with the encrypted PW
   // Once the user is set, call next and head back into the userController to save it to the DB
+  if (!password) res.status(422).json({ error: 'Please provide password' });
+  bcrypt.hash(password, SaltRounds)
+    .then((passwordHash) => {
+      if (!passwordHash) res.status(422).json({ error: 'Encrypt password failed' });
+      req.user = { username, password: passwordHash };
+      next();
+    })
+    .catch((error) => {
+      res.status(422).json({ error: error.message });
+    });
 };
 
 const compareUserPW = (req, res, next) => {
@@ -35,6 +45,20 @@ const compareUserPW = (req, res, next) => {
   // You'll need to find the user in your DB
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
   // If the passwords match set the username on `req` ==> req.username = user.username; and call next();
+  if (!username || !password) res.status(422).json({ error: 'Please provide username and password' });
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) res.status(422).json({ error: 'User not found' });
+      return bcrypt.compare(password, user.password);
+    })
+    .then((response) => {
+      if (!response) res.status(422).json({ error: 'Invalid password' });
+      req.username = username;
+      next();
+    })
+    .catch((error) => {
+      res.status(422).json({ error: error.message });
+    });
 };
 
 module.exports = {
